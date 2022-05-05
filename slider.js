4b8e2c2258ae54c8df139c9c44b7a5bd60cc21b0
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-05-05 07:45:19 +0200

    Add drag scroll

--- slider.js
+++ slider.js
@@ -1,10 +1,12 @@
 const container = document.getElementById("ctg_rss");
 const cards = document.getElementById("ctg_rss_content");
 const btnBack = document.getElementById("btn_back");
-const btnFront = document.getElementById("btn_front");
+const btnForw = document.getElementById("btn_front");
+const swipeBlock = document.getElementById("ctg_swipe_block");
 
 const THREE_BL_WIDTH = 800;
 const ONE_BL_WIDTH = 550;
+const JUMP_RANGE = 100;
 
 let elemWidth;
 let offsetLeft = 0;
@@ -48,13 +50,51 @@ btnBack.addEventListener("click", () => {
   cards.style.left = `${-offsetLeft}px`;
 });
 
-btnFront.addEventListener("click", () => {
+btnForw.addEventListener("click", () => {
   const container_rect = container.getBoundingClientRect();
   const cards_rect = cards.getBoundingClientRect();
-
   if (cards_rect.right - 50 > container_rect.right) {
     offsetLeft += elemWidth;
   } else offsetLeft = 0;
 
   cards.style.left = `${-offsetLeft}px`;
 });
+
+// **** Swipe block logic **** //
+
+let isPressedDown = false;
+let cursorXSpace;
+
+swipeBlock.addEventListener("mousedown", (e) => {
+  container.style.cursor = "grabbing";
+  cursorXSpace = e.offsetX - cards.offsetLeft;
+  isPressedDown = true;
+});
+
+swipeBlock.addEventListener("mouseup", () => {
+  container.style.cursor = "grab";
+});
+
+window.addEventListener("mouseup", () => {
+  isPressedDown = false;
+});
+
+swipeBlock.addEventListener("mousemove", (e) => {
+  const container_rect = container.getBoundingClientRect();
+  const cards_rect = cards.getBoundingClientRect();
+  if (!isPressedDown) return;
+  e.preventDefault();
+
+  let newValue = e.offsetX - cursorXSpace;
+  console.log(newValue, (cards_rect.width - container_rect.width) * -1);
+  if (newValue < JUMP_RANGE && newValue > (cards_rect.width - container_rect.width + JUMP_RANGE) * -1) {
+    offsetLeft = newValue;
+    cards.style.left = `${offsetLeft}px`;
+  }
+  setTimeout(alignVisBlock, 200);
+});
+
+function alignVisBlock() {
+  offsetLeft = Math.round(offsetLeft / elemWidth) * elemWidth;
+  cards.style.left = `${offsetLeft}px`;
+}
