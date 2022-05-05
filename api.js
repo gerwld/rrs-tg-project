8bcb77ce839f6fc309e6f608086c31806a7dc927
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-05-05 11:07:46 +0200

    Add rss parser & elem builder

--- api.js
+++ api.js
@@ -1,17 +1,47 @@
-// const getUsers = () => {
-//  axios.get('https://rsshub.app/telegram/channel/haregakaniti')
-//  .then(response => {
-//   const users = response.data.data;
-//   console.log(`GET users`, users);
-// })
-//  .catch(error => console.error(error));
-// };
-// getUsers();
+const loader = document.getElementById('ctg_load');
+const content = document.getElementById('ctg_rss_content');
+const instance = axios.create({
+  baseURL: "https://ctg-cors.herokuapp.com/https://rsshub.app/",
+});
 
 
-const BASE_URL = 'https://ctg-cors.herokuapp.com/https://rsshub.app/telegram/channel/haregakaniti';
+const getRssData = () => {
+  instance
+    .get("telegram/channel/haregakaniti")
+    .then((r) => {
+      const data = xmlToJSON.parseString(r.data);
+      console.log(`GET items`, data.rss[0].channel[0].item);
+      loader.classList.add('loaded');
+      createElemBuilder();
+      createElemBuilder();
+    })
+    .catch((error) =>{
+     loader.classList.add('error');
+     loader.querySelector('span').innerHTML = error.message;
+      console.error(error)
+     });
+};
+getRssData();
 
-fetch(BASE_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => console.log(data))
+
+const createElemBuilder = (img = "--", title ="--", pg ="--", date ="--") => {
+ var tag = document.createElement("div");
+ tag.classList.add('slide_block');
+ tag.innerHTML = `
+ <div class="slide_block_content">
+ <div class="image">
+   <img
+     src=${img}
+     alt=${title}>
+ </div>
+ <div class="content">
+   <h3>${title}</h3>
+   <p>${pg}</p>
+   <span class="datetime">${date}</span>
+ </div>
+ </div>
+ `
+ content.appendChild(tag);
+}
+
+
